668b858b8227e52c5ce98b90cbbc3eb733818613
Rahib <heydarov.rahib@gmail.com>
2021-04-24 16:52:13 -0400

    Yayyy

--- src/github_Day01/Hello_World.java
+++ src/github_Day01/Hello_World.java
@@ -4,5 +4,6 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Adding GIthUB NOw");
+        System.out.println("Yaaayyyy Stuff is on GitHub");
     }
 }
